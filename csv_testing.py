03b41501adcc80ccf35d9696452e6b4a560e6a6d
Robert Schaffer <rschaffer92@gmail.com>
2026-02-26 10:34:25 -0500

    Lesson 1-1 complete

--- csv_testing.py
+++ csv_testing.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('TkAgg') 
 
 def get_max_close(symbol):
     df = pd.read_csv("data/{}.csv".format(symbol))
@@ -16,9 +20,24 @@ def test_run():
     #     print("Max close")
     #     print(symbol, get_max_close(symbol))
 
-    for symbol in ['AAPL', 'IBM']:
-        print("Mean Volume")
-        print(symbol, get_mean_volume(symbol))
+    # for symbol in ['AAPL', 'IBM']:
+    #     print("Mean Volume")
+    #     print(symbol, get_mean_volume(symbol))
+
+    # df = pd.read_csv("data/AAPL.csv")
+    # print(df['Adj Close'])
+    # df["Adj Close"].plot()
+    # plt.show()
+
+    # df = pd.read_csv("data/IBM.csv")
+    # print(df['High'])
+    # df["High"].plot()
+    # plt.show()
+
+    df = pd.read_csv("data/AAPL.csv")
+    print(df[['Close','Adj Close']])
+    df[['Close','Adj Close']].plot()
+    plt.show()
 
 if __name__ == "__main__":
     test_run()
