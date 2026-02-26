60abd893750f3c708f9ea87a3e4b7c697bbcb13c
Robert Schaffer <rschaffer92@gmail.com>
2026-02-26 10:00:00 -0500

    initial commit

---
+++ csv_testing.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def get_max_close(symbol):
+    df = pd.read_csv("data/{}.csv".format(symbol))
+    return df['Close'].max()
+
+def get_mean_volume(symbol):
+    df = pd.read_csv("data/{}.csv".format(symbol))
+    return df['Volume'].mean()
+
+def test_run():
+    #df = pd.read_csv("data/AAPL.csv")
+    #print(df[10:21])
+
+    # for symbol in ['AAPL', 'IBM']:
+    #     print("Max close")
+    #     print(symbol, get_max_close(symbol))
+
+    for symbol in ['AAPL', 'IBM']:
+        print("Mean Volume")
+        print(symbol, get_mean_volume(symbol))
+
+if __name__ == "__main__":
+    test_run()
